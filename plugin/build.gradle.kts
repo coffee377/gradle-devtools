e78bd5375b22870dbd24f7369db8e78588892674
Wu Yujie <coffee377@dingtalk.com>
2022-10-18 22:53:05 +0800

    :tada: init

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    `java-gradle-plugin`
+    `embedded-kotlin`
+}
+
+repositories {
+    mavenLocal()
+    maven { url = uri("https://maven.aliyun.com/repository/public/") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+    maven { url = uri("https://maven.aliyun.com/repository/spring") }
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+}
+
+dependencies {
+//    implementation("org.junit.jupiter:junit-jupiter:5.7.2")
+}
+
+
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use JUnit Jupiter test framework
+            useJUnitJupiter("5.7.2")
+        }
+
+        // Create a new test suite
+        val functionalTest by registering(JvmTestSuite::class) {
+            dependencies {
+                // functionalTest test suite depends on the production code in tests
+                implementation(project)
+            }
+
+            targets {
+                all {
+                    // This test suite should run after the built-in test suite has run its tests
+                    testTask.configure { shouldRunAfter(test) }
+                }
+            }
+        }
+    }
+}
+
+gradlePlugin {
+    plugins {
+        creating {
+            id = "com.voc.gradle.plugin.greeting"
+            implementationClass = "com.voc.gradle.plugin.GradleDevtoolsPlugin"
+        }
+    }
+}
+
+gradlePlugin.testSourceSets(sourceSets["functionalTest"])
+
+tasks.named<Task>("check") {
+    // Include functionalTest as part of the check lifecycle
+    dependsOn(testing.suites.named("functionalTest"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "gradle-devtools"
+include("plugin")
